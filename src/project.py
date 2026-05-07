2885362904b7193c65dc84210bd7717d854a128f
Emily Reglado Lopez <Marzhq45@gmail.com>
2026-05-07 11:37:51 -0500

    I defined load_images which basically uses a for loop and goes through each frame/sprite sheet since they are all in the same page/sheet

--- src/project.py
+++ src/project.py
@@ -84,6 +84,17 @@ class Fighter():
         self.alive = True
         self.health = 100
 
+    def load_images(self, sprite_sheet, animation_steps):
+
+        animation_list = []
+        for y, animation in enumerate(animation_steps):
+            temp_img_list = []
+            for x in range(animation):
+                temp_img = sprite_sheet.subsurface(x * self.size, y * self.size, self.size, self.size)
+                temp_img_list.append(pygame.transform.scale(temp_img, (self.size * self.image_scale, self.size * self.image_scale)))
+            animation_list.append(temp_img_list)
+        return animation_list
+
     def move(self, screen_width, screen_height, surface, target, round_over):
         SPEED = 10
         GRAVITY = 2
